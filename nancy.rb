bf4ac8b448adadee5a5297a6df5519d4827f18aa
Daisuke Ito <itodais113@gmail.com>
2015-10-09 14:06:05 +0900

    Delegating to Nancy::Application

--- nancy.rb
+++ nancy.rb
@@ -47,32 +47,33 @@ module Nancy
       end
     end
 
+    private
+
     def params
       @request.params
     end
-
-    private
 
     def route(verb, path, &handler)
       @routes[verb] ||= {}
       @routes[verb][path] = handler
     end
   end
+
+  Application = Base.new
+
+  module Delegator
+    def self.delegate(*methods, to:)
+      Array(methods).each do |method_name|
+        define_method(method_name) do |*args, &block|
+          to.send(method_name, *args, &block)
+        end
+
+        private method_name
+      end
+    end
+
+    delegate :get, :patch, :put, :post, :delete, :head, to: Application
+  end
 end
 
-nancy = Nancy::Base.new
-
-nancy.get "/hello" do
-  "Nancy says hello"
-end
-
-nancy.get "/" do
-  [200, {}, ["Your params are #{params.inspect}"]]
-end
-
-nancy.post "/" do
-  [200, {}, request.body]
-end
-
-# This line is new!
-Rack::Handler::WEBrick.run nancy, Port: 9292
+include Nancy::Delegator
