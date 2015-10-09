25bb034fa0324cb8b67caca2154a596c382a12e9
Daisuke Ito <itodais113@gmail.com>
2015-10-09 13:47:43 +0900

    Modern conveniences

--- nancy.rb
+++ nancy.rb
@@ -1,4 +1,4 @@
-require 'rack'
+require "rack"
 
 module Nancy
   class Base
@@ -6,10 +6,49 @@ module Nancy
       @routes = {}
     end
 
-    attr_reader :routes
+    attr_reader :routes, :request
 
     def get(path, &handler)
       route("GET", path, &handler)
+    end
+
+    def post(path, &handler)
+      route("POST", path, &handler)
+    end
+
+    def put(path, &handler)
+      route("PUT", path, &handler)
+    end
+
+    def patch(path, &handler)
+      route("PATCH", path, &handler)
+    end
+
+    def delete(path, &handler)
+      route("DELETE", path, &handler)
+    end
+
+    def call(env)
+      @request = Rack::Request.new(env)
+      verb = @request.request_method
+      requested_path = @request.path_info
+
+      handler = @routes.fetch(verb, {}).fetch(requested_path, nil)
+
+      if handler
+        result = instance_eval(&handler)
+        if result.class == String
+          [200, {}, [result]]
+        else
+          result
+        end
+      else
+        [404, {}, ["Oops! No route for #{verb} #{requested_path}"]]
+      end
+    end
+
+    def params
+      @request.params
     end
 
     private
@@ -24,7 +63,16 @@ end
 nancy = Nancy::Base.new
 
 nancy.get "/hello" do
-  [200, {}, ["Nancy says helo"]]
+  "Nancy says hello"
 end
 
-puts nancy.routes
+nancy.get "/" do
+  [200, {}, ["Your params are #{params.inspect}"]]
+end
+
+nancy.post "/" do
+  [200, {}, request.body]
+end
+
+# This line is new!
+Rack::Handler::WEBrick.run nancy, Port: 9292
